d3d0fda4a2f025366ab2c71622246b42abfd0cc8
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-06-22 00:18:40 -0400

    Transformar botão em componente

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
+import { ButtonHTMLAttributes } from "react";
 
-export function Button() {
-  const [counter, setCounter] = useState(0)
-  
-  function increment() {
-    // Conceito de imutabilidade -> não modificamos um counter,
-    // sempre um novo counter é criado a partir do valor anterior
-    setCounter(counter + 1)
-  }
-
-  return (
-    <button onClick={increment}>
-      {counter}
-    </button>
-  )
-}
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 // Named Export
+export function Button(props: ButtonProps) {
+  // Conceito de imutabilidade -> não modificamos um counter,
+  // sempre um novo counter é criado a partir do valor anterior
+  
+  return (
+    <button className="button" {...props} />
+  )
+}
