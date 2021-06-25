be7579d93cf015afb4ff384cd91103e14208ce9d
Thiago Santos Borges <tsb.snf16@uea.edu.br>
2021-06-25 15:02:44 -0400

    NLW finish

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -2,6 +2,8 @@ import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
 import deleteImg from '../assets/images/delete.svg';
+import answerImg from '../assets/images/answer.svg';
+import checkImg from '../assets/images/check.svg';
 
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
@@ -35,10 +37,22 @@ export function AdminRoom() {
     history.push('/');
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isAnswered: true,
+    });
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+      isHighLighted: true,
+    });
+  }
+
+
   async function handleDeleteQuestion(questionId: string) {
     if (window.confirm("Tem certeza que você deseja excluir esta pergunta?")) {
-      const questionRef = await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
-
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
   }
 
@@ -66,7 +80,25 @@ export function AdminRoom() {
               key={question.id}
               content={question.content}
               author={question.author}
+              isAnswered={question.isAnswered}
+              isHighLighted={question.isHighLighted}
             >
+              {!question.isAnswered && (
+                <>
+                  <button
+                    type="button"
+                    onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                  >
+                    <img src={checkImg} alt="Marcar Pergunta como respondida"></img>
+                  </button>
+                  <button
+                    type="button"
+                    onClick={() => handleHighlightQuestion(question.id)}
+                  >
+                    <img src={answerImg} alt="Dar destaque à Pergunta"></img>
+                  </button>
+                </>
+              )}
               <button
                 type="button"
                 onClick={() => handleDeleteQuestion(question.id)}
@@ -79,8 +111,5 @@ export function AdminRoom() {
       </main>
     </div>
   );
-}
-function useHIstory() {
-  throw new Error('Function not implemented.');
 }
 
